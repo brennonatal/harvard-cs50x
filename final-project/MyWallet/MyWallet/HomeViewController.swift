72d28bc474af7acd5d056861d222e838d09030d4
Brenno Natal <natalbrenno@gmail.com>
2020-12-16 19:08:49 -0300

    Listing user transactions

--- final-project/MyWallet/MyWallet/HomeViewController.swift
+++ final-project/MyWallet/MyWallet/HomeViewController.swift
@@ -10,8 +10,46 @@ import Firebase
 
 class HomeViewController: UITableViewController {
     
+    var userUid: String!
+    var transactions: [Transaction] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        userUid = Auth.auth().currentUser!.uid
+        reload()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        reload()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return transactions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TransactionCell", for: indexPath)
+        cell.textLabel?.text = transactions[indexPath.row].name
+//        ~~~~~~~~~~~~~~ CHANGE COLOR OF CELLS HERE ~~~~~~~~~~~~~~
+        return cell
+    }
+    
+    func reload() {
+        transactions = TransactionManager.main.getUserTransactions(uid: userUid)
+        self.tableView.reloadData()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "TransactionSegue" {
+            if let destination = segue.destination as? TransactionViewController {
+                destination.transaction = transactions[tableView.indexPathForSelectedRow!.row]
+            }
+        }
     }
     
     @IBAction func confirmLogOut() {
