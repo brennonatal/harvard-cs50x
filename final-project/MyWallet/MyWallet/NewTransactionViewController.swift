6a1ab7a3c912b6cb19d49412abf128b762c4062d
Brenno Natal <natalbrenno@gmail.com>
2020-12-17 16:10:05 -0300

    Using UIPickerView for type selection

--- final-project/MyWallet/MyWallet/NewTransactionViewController.swift
+++ final-project/MyWallet/MyWallet/NewTransactionViewController.swift
@@ -8,55 +8,20 @@
 import UIKit
 import Firebase
 
-class NewTransactionViewController: UIViewController /*, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate */{
+class NewTransactionViewController: UIViewController {
     
     var userUid: String!
-    
-//    var selectedType: String?
-//    var typeList = ["Income", "Outcome"]
-//
-//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-//        return 1
-//    }
-//
-//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-//        return typeList.count
-//    }
-//
-//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-//        return typeList[row]
-//    }
-//
-//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-//        selectedType = typeList[row]
-//        type.text = selectedType
-//    }
-//
-//    func createPickerView() {
-//           let pickerView = UIPickerView()
-//           pickerView.delegate = self
-//           type.inputView = pickerView
-//    }
-//
-//    func dismissPickerView() {
-//       let toolBar = UIToolbar()
-//       toolBar.sizeToFit()
-//       let button = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.action))
-//       toolBar.setItems([button], animated: true)
-//       toolBar.isUserInteractionEnabled = true
-//       type.inputAccessoryView = toolBar
-//    }
-//
-//    @objc func action() {
-//          view.endEditing(true)
-//    }
+    let typeData = ["Income", "Outcome"]
     
     @IBOutlet var name: UITextField!
     @IBOutlet var amount: UITextField!
-    @IBOutlet var type: UITextField!
+    @IBOutlet var type: UIPickerView!
+    var textType: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        type.dataSource = self
+        type.delegate = self
         userUid = Auth.auth().currentUser?.uid
     }
     
@@ -64,12 +29,27 @@ class NewTransactionViewController: UIViewController /*, UIPickerViewDelegate, U
         if name.text == "" {
             name.text = "No name"
         }
-        if type.text == "" {
-            type.text = "Not provided"
-        }
-        
-        let _ = TransactionManager.main.create(uid: userUid, name: name.text!, amount: Double(amount.text ?? "0") ?? 0, type: type.text!)
+        let _ = TransactionManager.main.create(uid: userUid, name: name.text!, amount: Double(amount.text ?? "0") ?? 0, type: textType!)
         self.navigationController?.popViewController(animated: true)
     }
     
+}
+
+extension NewTransactionViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return typeData.count
+    }
+    
+    
+}
+
+extension NewTransactionViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        textType = typeData[row]
+        return typeData[row]
+    }
 }
--- final-project/MyWallet/MyWallet/TransactionViewController.swift
+++ final-project/MyWallet/MyWallet/TransactionViewController.swift
@@ -7,60 +7,35 @@
 
 import UIKit
 
-class TransactionViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
-
+class TransactionViewController: UIViewController {
     
     var transaction: Transaction!
-    
-    var selectedType: String?
-    var typeList = ["Income", "Outcome"]
-
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return typeList.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return typeList[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        selectedType = typeList[row]
-        type.text = selectedType
-    }
-    
-    func createPickerView() {
-           let pickerView = UIPickerView()
-           pickerView.delegate = self
-           type.inputView = pickerView
-    }
-    
-    func dismissPickerView() {
-       let toolBar = UIToolbar()
-       toolBar.sizeToFit()
-       let button = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.action))
-       toolBar.setItems([button], animated: true)
-       toolBar.isUserInteractionEnabled = true
-       type.inputAccessoryView = toolBar
-    }
-    
-    @objc func action() {
-          view.endEditing(true)
-    }
+    var typeData: [String]!
+    var textType: String!
     
     @IBOutlet var name: UITextField!
     @IBOutlet var amount: UITextField!
-    @IBOutlet var type: UITextField!
+    @IBOutlet var type: UIPickerView!
     @IBOutlet var date: UITextField!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        if transaction.type == "Income" {
+            typeData = ["Income", "Outcome"]
+        }
+        else {
+            typeData = ["Outcome", "Income"]
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        type.dataSource = self
+        type.delegate = self
+        
         name.text = transaction.name
         amount.text = String(transaction.amount)
-        type.text = transaction.type
         date.text = transaction.date
     }
     
@@ -69,9 +44,27 @@ class TransactionViewController: UIViewController, UIPickerViewDelegate, UIPicke
         
         transaction.name = name.text ?? "No name"
         transaction.amount = Double(amount.text ?? "0") ?? 0
-        transaction.type = type.text!
-        
+        transaction.type = textType
         TransactionManager.main.update(transaction: transaction)
     }
     
+}
+
+extension TransactionViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return typeData.count
+    }
+    
+    
+}
+
+extension TransactionViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        textType = typeData[row]
+        return typeData[row]
+    }
 }
